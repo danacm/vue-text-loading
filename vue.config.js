2a1d9a0fa8be237991bab5b1cfeef5fe45932e8a
danacm <chen_dana@sina.cn>
2020-08-03 11:00:27 +0800

    change build type

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
+  devServer: {
+    port: '9100',
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
   productionSourceMap: false,
   outputDir: process.env.outputDir,
-  publicPath:process.env.publicPath
-}
+  publicPath: process.env.publicPath,
+  css: {
+    extract: false
+  }
+};
